63713f2ee5170ff14dd26389a923dc6c8b2c9fea
Sanket Shah <shahsanket322003@gmail.com>
2026-08-25 03:09:27 +0000

    feat: doctype creation

---
+++ warehouse_management/warehouse_management/doctype/warehouse_daily_assignment/warehouse_daily_assignment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Reformiqo and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Warehouse Daily Assignment", {
+// 	refresh(frm) {
+
+// 	},
+// });
