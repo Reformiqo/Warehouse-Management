99d39e5acb8443b7d8d05345d21d261065d530cb
Sanket Shah <shahsanket322003@gmail.com>
2026-09-10 16:13:53 +0000

    fix: remove extra code while installing

--- warehouse_management/setup/custom_fields.py
+++ warehouse_management/setup/custom_fields.py
@@ -205,8 +205,4 @@ def get_custom_fields():
 
 
 def create_fields():
-	# Warehouse, Sales Order and friends carry orphan link fields from an
-	# uninstalled app, and saving a Custom Field revalidates the whole doctype,
-	# which would throw on them and abort setup before anything is created
-	create_custom_fields(get_custom_fields(), ignore_validate=True)
-	frappe.db.commit()
+	create_custom_fields(get_custom_fields())
